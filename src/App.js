edd923ee09d02f65aa0b2d1d58076dc06e6b4c53
x1mmy <zimraan2012@gmail.com>
2021-11-16 20:22:42 +1100

    'Enter'

--- src/App.js
+++ src/App.js
@@ -1,64 +1,109 @@
-import { useState} from 'react'
+import { useState,  useEffect } from 'react'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
 import { FaTrashAlt } from 'react-icons/fa';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
+
+
 function App() {
   const [showAddTasks, setShowAddTasks] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctor',
-        day: 'Feb 5 at 2.30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'School',
-        day: 'Feb 6 at 1.30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Shopping',
-        day: 'Feb 4 at 3.30pm',
-        reminder: false,
-    },
-])
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  //Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch ('http://localhost:5000/tasks')
+    const data = await res.json()
+    
+    return data
+  }
+
+    //Fetch Task
+    const fetchTask = async (id) => {
+      const res = await fetch (`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+      
+      return data
+    }
 
 //Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() *
-  1000) + 1
-  const newtask = {id, ...task}
-  setTasks([...tasks, newtask])
+const addTask = async (task) => {
+  const res = await fetch(`http://localhost:5000/tasks`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body:JSON.stringify(task)
+  })
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+  
+  // const id = Math.floor(Math.random() *
+  // 1000) + 1
+  // const newtask = {id, ...task}
+  // setTasks([...tasks, newtask])
   
 }
 
   //Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch (`http://localhost:5000/tasks/${id}`,{
+      method: 'DELETE'
+    })
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
 
   //Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
     setTasks(
       tasks.map((task) => 
-        task.id === id ? { ...task, reminder: !task.reminder} : task
+        task.id === id ? { ...task, reminder:
+        !task.reminder} : task
     )
   )
   }
 
   return (
-    <div className='container'>
-      <Header 
-        onAdd={() => setShowAddTasks(!showAddTasks)} showAdd={showAddTasks}
-      /> 
-      {showAddTasks && <AddTask onAdd = {addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks}  onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to Show'}
-    </div>
+    <Router>
+      <div className='container'>
+        <Header 
+          onAdd={() => setShowAddTasks(!showAddTasks)} showAdd={showAddTasks}
+        /> 
+        {showAddTasks && <AddTask onAdd = {addTask} />}
+        {tasks.length > 0 ? <Tasks tasks={tasks}  onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to Show'}
+        
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p><b>How to use: </b><br />
+                - Press Add in order to place a new task
+                <br />
+                - Press the cross in order to delete a task
+                <br />
+                - Double click a task in order to turn a reminder on or off 
+            </p>
+            <br />
+            <br />
+            <p>Copyright &copy; 2021</p>
+        </footer>
+    )
+}
+
+export default Footer
